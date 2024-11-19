6bf1301066481bbdbb0f87a21a4f0c4b42ea5077
TeKayy <th3tk.idk@gmail.com>
2024-11-19 20:04:07 +0100

    ^ for last commit

--- react-admin-panel/src/config/envValidation.js
+++ react-admin-panel/src/config/envValidation.js
@@ -1,10 +1,21 @@
 export default [
     {
         path: 'VITE_API_BASE_URL',
-        regex: /^https?:\/\/[\w\-.~!*'();:@&=+$,/?%#\[\]]*(%[0-9a-fA-F]{2})*(:\d{2,5})?$/
+        regex: /^https?:\/\/[\w\-.~!*'();:@&=+$,/?%#\[\]]*(%[0-9a-fA-F]{2})*(:\d{2,5})?$/,
     },
     {
         path: 'VITE_API_WEBSOCKET_URL',
-        regex: /^wss?:\/\/[\w\-.~!*'();:@&=+$,/?%#\[\]]*(%[0-9a-fA-F]{2})*(:\d{2,5})?$/
+        regex: /^wss?:\/\/[\w\-.~!*'();:@&=+$,/?%#\[\]]*(%[0-9a-fA-F]{2})*(:\d{2,5})?$/,
+    },
+    {
+        path: 'VITE_TRAEFIK_PANEL_URL',
+        regex: /^https?:\/\/[\w\-.~!*'();:@&=+$,/?%#\[\]]*(%[0-9a-fA-F]{2})*(:\d{2,5})?$/,
+        optional: true,
+
+    },
+    {
+        path: 'VITE_GUACAMOLE_PANEL_URL',
+        regex: /^https?:\/\/[\w\-.~!*'();:@&=+$,/?%#\[\]]*(%[0-9a-fA-F]{2})*(:\d{2,5})?$/,
+        optional: true,
     }
 ]
