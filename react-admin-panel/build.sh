ff9f376ab6adfd1dbf84397aa557b057f75c2a33
Krzysztoff27 <krzysztof.kolasinski27@outlook.com>
2024-11-24 18:24:19 +0100

    Modified dockerfile and build.sh scripts in accordance with FastAPI requirements for passing environmental variables. Fixed network definition in docker-compose.yaml.

--- react-admin-panel/build.sh
+++ react-admin-panel/build.sh
@@ -1,2 +1,18 @@
 #!/usr/bin/env bash
-docker build . -t cherry-admin-panel -f docker/dockerfile
+if [[ $# -lt 4 ]]; then
+    printf "Usage: %s <VITE_API_BASE_URL> <VITE_API_WEBSOCKET_URL> <VITE_TRAEFIK_PANEL_URL> <VITE_GUACAMOLE_PANEL_URL>" "$0"
+    exit 1
+fi
+
+VITE_API_BASE_URL=$1
+VITE_API_WEBSOCKET_URL=$2
+VITE_TRAEFIK_PANEL_URL=$3
+VITE_GUACAMOLE_PANEL_URL=$4
+
+docker build \
+    -f docker/dockerfile \
+    --build-arg VITE_API_BASE_URL="$VITE_API_BASE_URL" \
+    --build-arg VITE_API_WEBSOCKET_URL="$VITE_API_WEBSOCKET_URL" \
+    --build-arg VITE_TRAEFIK_PANEL_URL="$VITE_TRAEFIK_PANEL_URL" \
+    --build-arg VITE_GUACAMOLE_PANEL_URL="$VITE_GUACAMOLE_PANEL_URL" \
+    -t cherry-admin-panel .
