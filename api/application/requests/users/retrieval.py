13f3a699b00cd8b25b64b514572dabfb47bbbca2
TeKayy <th3tk.idk@gmail.com>
2025-03-12 12:26:19 +0100

    Corrected the response models in the user retrieval methods.

--- api/application/requests/users/retrieval.py
+++ api/application/requests/users/retrieval.py
@@ -2,12 +2,12 @@ from application import app
 from application.users import User, Filters, get_user_by_uuid, get_filtered_users, AccountTypes, Administrator, Client
 from application.authentication import DependsOnAuthentication
 
-@app.get("/user", response_model=User, tags=['users'])
-async def __read_user_me__(current_user: DependsOnAuthentication) -> User:
+@app.get("/user", response_model=Administrator | Client, tags=['users'])
+async def __read_user_me__(current_user: DependsOnAuthentication) -> Administrator | Client:
     return current_user
 
-@app.get("/user/{uuid}", response_model=User, tags=['users'])
-async def __read_user__(uuid: str, current_user: DependsOnAuthentication) -> User:
+@app.get("/user/{uuid}", response_model=Administrator | Client, tags=['users'])
+async def __read_user__(uuid: str, current_user: DependsOnAuthentication) -> Administrator | Client:
     return get_user_by_uuid(uuid)
 
 @app.get("/users", response_model=dict[str, Administrator | Client], tags=['users'])
