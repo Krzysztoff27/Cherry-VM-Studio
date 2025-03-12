188bafe86244be0fe4598c772dc7feeeb126d76f
TeKayy <th3tk.idk@gmail.com>
2025-03-12 12:27:37 +0100

    Introduced translation for the TableStateHeading.

--- admin-panel/src/components/molecules/feedback/TableStateHeading/TableStateHeading.tsx
+++ admin-panel/src/components/molecules/feedback/TableStateHeading/TableStateHeading.tsx
@@ -1,25 +1,28 @@
 import { Group, Title } from "@mantine/core";
 import React from "react";
+import useNamespaceTranslation from "../../../../hooks/useNamespaceTranslation";
 
 const TableStateHeading = ({ getIsSomeRowsSelected, getIsAllRowsSelected, getCoreRowModel, getSelectedRowModel, getRowCount }): React.JSX.Element => {
+    const { tns } = useNamespaceTranslation("pages");
 
     const areRowsSelected = getIsSomeRowsSelected() || getIsAllRowsSelected();
     const areRowsFiltered = getCoreRowModel().rows.length !== getRowCount();
 
-    const heading = areRowsSelected ? 'Selected accounts' : areRowsFiltered ? 'Filtered results' : 'All accounts';
-    const color = areRowsSelected ? 'cherry.2' : 'dimmed';
+    const heading = tns(`accounts.controls.${areRowsSelected ? "selected-accounts" : areRowsFiltered ? "filtered-results" : "all-accounts"}`);
+    const color = areRowsSelected ? "cherry.2" : "dimmed";
     const amount = areRowsSelected ? getSelectedRowModel().rows.length : getRowCount();
 
     return (
         <Group>
-            <Title order={2}>
-                {heading}
-            </Title>
-            <Title order={2} c={color}>
+            <Title order={2}>{heading}</Title>
+            <Title
+                order={2}
+                c={color}
+            >
                 {amount}
             </Title>
         </Group>
     );
-}
+};
 
 export default TableStateHeading;
