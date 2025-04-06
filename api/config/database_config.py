426db7bf03e9c3b2dc0addacd4577f8e466e5387
Krzysztoff27 <krzysztof.kolasinski27@outlook.com>
2025-04-06 13:40:30 +0200

    Updated Apache Guacamole db credentials.

--- api/config/database_config.py
+++ api/config/database_config.py
@@ -4,11 +4,11 @@ from pydantic import BaseModel
 
 @dataclass(frozen=True)
 class DatabaseConfig:
-    host: str = "localhost"
-    port: str = "9000"
-    user: str = "postgres"
-    password: str = "qazwsx"
-    dbname: str = "cvmm-dev"
+    host: str = "cherry-guac-db"
+    port: str = "5432"
+    user: str = "guac"
+    password: str = "guacamole"
+    dbname: str = "guacamole_db"
     max_connections: int = 20    
     timeout_seconds: float = 5   # request timeout
     
