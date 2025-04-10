3ca1407660e1aa619509321a483988a0fdadb0db
TeKayy <th3tk.idk@gmail.com>
2025-04-10 19:28:06 +0200

    Implemented creation of default administrative account + roles in the database initialization script.

--- backend/docker/apache-guacamole/initdb/02-initdb.sql
+++ backend/docker/apache-guacamole/initdb/02-initdb.sql
@@ -2,31 +2,37 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE administrators (
     uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    username VARCHAR(24) UNIQUE,
+    username VARCHAR(24) UNIQUE NOT NULL,
     password VARCHAR(60),
     name VARCHAR(50),
     surname VARCHAR(50),
     email VARCHAR(255) UNIQUE
+    creation_date DATE NOT NULL DEFAULT current_date,
+    last_active TIMESTAMP,
+    disabled BOOLEAN DEFAULT 0
 );
 
 CREATE TABLE clients (
     uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    username VARCHAR(24) UNIQUE,
+    username VARCHAR(24) UNIQUE NOT NULL,
     password VARCHAR(60),
     name VARCHAR(50),
     surname VARCHAR(50),
-    email VARCHAR(255) UNIQUE
+    email VARCHAR(255) UNIQUE,
+    creation_date DATE NOT NULL DEFAULT current_date,
+    last_active TIMESTAMP,
+    disabled BOOLEAN DEFAULT 0
 );
 
 CREATE TABLE roles (
     uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(50),
-    permissions INT
+    name VARCHAR(50) UNIQUE,
+    permissions INT DEFAULT 0
 );
 
 CREATE TABLE groups (
     uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(50)
+    name VARCHAR(50) UNIQUE
 );
 
 CREATE TABLE administrators_roles (
@@ -49,3 +55,31 @@ CREATE INDEX administrators_idx ON administrators (uuid, username, email);
 CREATE INDEX clients_idx ON clients (uuid, username, email);
 CREATE INDEX roles_idx ON roles (uuid);
 CREATE INDEX groups_idx ON groups (uuid);
+CREATE INDEX administrators_roles_idx ON administrators_roles (administrator_uuid, role_uuid);
+CREATE INDEX clients_groups_idx ON clients_groups (client_uuid, group_uuid);
+
+-- Insert roles
+INSERT INTO roles (name, permissions)
+VALUES 
+    ('Machine Observer', 1),
+    ('Machine Manager', 3),
+    ('Client Accounts Manager', 4),
+    ('Administrative Accounts Manager', 8),
+    ('Accounts Manager', 12),
+    ('Client Credentials Manager', 16),
+    ('Administrative Credentials Manager', 32),
+    ('Credentials Manager', 48);
+
+-- Insert root administrator
+INSERT INTO administrators (uuid, username, password)
+VALUES 
+    ('83212b1e-b222-4bba-a1d4-450e08cbbeb1', 'root', '$2b$12$GNkVdiV24DIhBWYssz0H9.22nhoI1EuT9TNXRpwUHOCRKyKa7.wfS');
+
+-- Assign roles to root administrator
+INSERT INTO administrators_roles (administrator_uuid, role_uuid)
+SELECT 
+    '83212b1e-b222-4bba-a1d4-450e08cbbeb1',
+    uuid
+FROM roles
+WHERE name IN ('Machine Manager', 'Accounts Manager', 'Credentials Manager');
+
