f6f785b4d6b6584521042dd4963f9364cdb3d994
TeKayy <th3tk.idk@gmail.com>
2025-04-13 19:40:21 +0200

    Resolved issues with generated role descriptions.

--- admin-panel/src/components/atoms/display/RoleInfoCard/RoleInfoCard.tsx
+++ admin-panel/src/components/atoms/display/RoleInfoCard/RoleInfoCard.tsx
@@ -16,27 +16,26 @@ export default function RoleInfoCard({ role }: { role: RoleInDB }) {
 
     // for each group it chooses the first mask that matches
     const groupedPermissionKeys = [
-        {
-            [PERMISSIONS.MANAGE_ALL_VMS]: "permissions.manage-machines",
-            [PERMISSIONS.VIEW_ALL_VMS]: "permissions.view-machines",
-        },
-        {
-            [PERMISSIONS.MANAGE_CLIENT_USERS | PERMISSIONS.MANAGE_ADMIN_USERS]: "permissions.manage-all-accounts",
-            [PERMISSIONS.MANAGE_CLIENT_USERS]: "permissions.manage-client-accounts",
-            [PERMISSIONS.MANAGE_ADMIN_USERS]: "permissions.manage-administrative-accounts",
-        },
-        {
-            [PERMISSIONS.CHANGE_CLIENT_PASSWORD | PERMISSIONS.CHANGE_ADMIN_PASSWORD]: "permissions.modify-credentials",
-            [PERMISSIONS.CHANGE_CLIENT_PASSWORD]: "permissions.modify-clients-credentials",
-            [PERMISSIONS.CHANGE_ADMIN_PASSWORD]: "permissions.modify-administrators-credentials",
-        },
+        [
+            { mask: PERMISSIONS.MANAGE_ALL_VMS, label: "permissions.manage-machines" },
+            { mask: PERMISSIONS.VIEW_ALL_VMS, label: "permissions.view-machines" },
+        ],
+        [
+            { mask: PERMISSIONS.MANAGE_CLIENT_USERS | PERMISSIONS.MANAGE_ADMIN_USERS, label: "permissions.manage-all-accounts" },
+            { mask: PERMISSIONS.MANAGE_CLIENT_USERS, label: "permissions.manage-client-accounts" },
+            { mask: PERMISSIONS.MANAGE_ADMIN_USERS, label: "permissions.manage-administrative-accounts" },
+        ],
+        [
+            { mask: PERMISSIONS.CHANGE_CLIENT_PASSWORD | PERMISSIONS.CHANGE_ADMIN_PASSWORD, label: "permissions.modify-credentials" },
+            { mask: PERMISSIONS.CHANGE_CLIENT_PASSWORD, label: "permissions.modify-clients-credentials" },
+            { mask: PERMISSIONS.CHANGE_ADMIN_PASSWORD, label: "permissions.modify-administrators-credentials" },
+        ],
     ];
 
     groupedPermissionKeys.forEach(group => {
-        const found = safeObjectKeys(group).find((permission: number) => roleHasPermission(permission));
+        const found = group.find(({ mask }) => roleHasPermission(mask));
         if (found) {
-            console.log(found);
-            matched.push(tns(group[found]));
+            matched.push(tns(found.label));
         }
     });
 
