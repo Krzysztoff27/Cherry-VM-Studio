03d70133f06255e99b6c7c2086da93d740dec9b7
TeKayy <th3tk.idk@gmail.com>
2025-04-13 13:11:38 +0200

    Added required machine functions for the API to work. Implemented get_user_machine_uuids that returns uuids of all machines the user has ownership of.

--- api/application/machines/__init__.py
+++ api/application/machines/__init__.py
@@ -1,9 +1,10 @@
 from uuid import UUID
 
+from .models import MachineData
 from application.users.models import AdministratorInDB, ClientInDB
-from application.postgresql import select_schema_dict, select_schema_one
+from application.postgresql import select_schema_dict, select_schema_one, select_single_field
 
-def get_machines_owner(machine_uuid) -> AdministratorInDB | None:
+def get_machines_owner(machine_uuid: UUID) -> AdministratorInDB | None:
     return select_schema_one(AdministratorInDB, """
         SELECT administrators.* FROM administrators
         RIGHT JOIN deployed_machines_owners ON administrators.uuid = deployed_machines_owners.owner_uuid
@@ -11,13 +12,22 @@ def get_machines_owner(machine_uuid) -> AdministratorInDB | None:
     """, (machine_uuid,))
     
     
-def get_clients_assigned_to_machine(machine_uuid) -> dict[UUID, ClientInDB]:
+def get_clients_assigned_to_machine(machine_uuid: UUID) -> dict[UUID, ClientInDB]:
     return select_schema_dict(ClientInDB, "uuid", """
         SELECT clients.* FROM clients
         RIGHT JOIN deployed_machines_clients ON clients.uuid = deployed_machines_clients.client_uuid
         WHERE deployed_machines_clients.machine_uuid = %s
     """, (machine_uuid,))
     
+def get_user_machine_uuids(owner_uuid: UUID) -> list[UUID]:
+    return select_single_field("machine_uuid", "SELECT machine_uuid FROM deployed_machines_owners WHERE owner_uuid = %s", (owner_uuid,))
+
+    
+def get_machine(uuid) -> MachineData | None:
+    return None
+
+def get_all_machines() -> dict[UUID, MachineData]:
+    return {}
 
     
         
--- api/application/requests/machines/__init__.py
+++ api/application/requests/machines/__init__.py
@@ -1,27 +1,25 @@
 from uuid import UUID
 from application import app
-from application.machines import get_machine, get_machines
-from application.machines.models import MachineNetworkData
+from application.machines.models import MachineData
 from application.authentication import DependsOnAuthentication
-import libvirt
 
 ###############################
 #       data requests
 ###############################
 
-@app.get("/machines/global", response_model=dict[UUID, MachineNetworkData], tags=['Machine Data'])
-async def get_all_machines(current_user: DependsOnAuthentication) -> dict[UUID, MachineNetworkData]:
+@app.get("/machines/global", response_model=dict[UUID, MachineData], tags=['Machine Data'])
+async def get_all_machines(current_user: DependsOnAuthentication) -> dict[UUID, MachineData]:
 
     # should return all machines deployed in the CVMM
     return {}
 
-@app.get("/machines", response_model=dict[UUID, MachineNetworkData], tags=['Machine Data'])
-async def get_all_logged_in_users_vm_network_data__(current_user: DependsOnAuthentication) -> dict[UUID, MachineNetworkData]:
+@app.get("/machines", response_model=dict[UUID, MachineData], tags=['Machine Data'])
+async def get_all_logged_in_users_vm_network_data__(current_user: DependsOnAuthentication) -> dict[UUID, MachineData]:
     
     # should return current_user's machines
     return {}
 
-@app.get("/machine/{uuid}", response_model=dict[UUID, MachineNetworkData], tags=['Machine Data'])
-async def __get_vm_network_data__(uuid: str, current_user: DependsOnAuthentication) -> MachineNetworkData | None:  
+@app.get("/machine/{uuid}", response_model=MachineData | None, tags=['Machine Data'])
+async def __get_vm_network_data__(uuid: str, current_user: DependsOnAuthentication) -> MachineData | None:  
     
     return None
