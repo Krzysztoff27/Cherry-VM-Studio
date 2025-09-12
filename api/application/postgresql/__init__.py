cd8628998811e3d58762dbf84fcd06fe1d8d4073
TeKayy <th3tk.idk@gmail.com>
2025-09-12 14:57:23 +0200

    Fixed incorrect type annotations.

--- api/application/postgresql/__init__.py
+++ api/application/postgresql/__init__.py
@@ -6,6 +6,9 @@ from psycopg.rows import dict_row
 from pydantic import BaseModel
 from .models import Params
 from config import DATABASE_CONFIG
+from typing import Type, TypeVar, Optional
+
+T = TypeVar("T", bound=BaseModel)
 
 pool = psycopg_pool.ConnectionPool(
     conninfo=f"dbname={DATABASE_CONFIG.dbname} user={DATABASE_CONFIG.user} "
@@ -41,16 +44,16 @@ def select_single_field(key_name: str, query: str, params: Params | None = None)
 
 # select schema functions fetch data from the select query and validate it using the given pydantic model
 
-def select_schema(model: Type[BaseModel], query: str, params: Params | None = None) -> list[any]:
+def select_schema(model: Type[T], query: str, params: Params | None = None) -> list[any]:
     rows = select_rows(query, params)
     return [model.model_validate(row) for row in rows]
 
-def select_schema_dict(model: Type[BaseModel], key_name: str, query: str, params: Params | None = None) -> dict[str, any]:
+def select_schema_dict(model: Type[T], key_name: str, query: str, params: Params | None = None) -> dict[str, any]:
     rows = select_rows(query, params)
     return {row[key_name]: model.model_validate(row) for row in rows}
 
 # returns first row of the SELECT query validated with given model
-def select_schema_one(model: Type[BaseModel], query: str, params: Params | None = None) -> Type[BaseModel] | None:
+def select_schema_one(model: Type[T], query: str, params: Params | None = None) -> Optional[T]:
     row = select_one(query, params)
     if not row:
         return None
