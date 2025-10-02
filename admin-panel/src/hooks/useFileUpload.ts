5a1df9f9a393cacdea6ed5980ff116b406216b29
TeKayy <th3tk.idk@gmail.com>
2025-10-02 13:39:43 +0200

    Implemented a hook for uploading files.

---
+++ admin-panel/src/hooks/useFileUpload.ts
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { UploadStatus } from "../types/hooks.types";
+import { isNull } from "lodash";
+import axios from "axios";
+import useAuth from "./useAuth";
+
+const useFileUpload = () => {
+    const [status, setStatus] = useState<UploadStatus>("idle");
+    const [progress, setProgress] = useState(0);
+    const { tokens } = useAuth();
+
+    const uploadFile = async (file: File | null) => {
+        console.log(file);
+        if (isNull(file)) return;
+
+        setStatus("uploading");
+
+        const formData = new FormData();
+        formData.append("file", file);
+
+        try {
+            await axios.post("https://file.io", formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+                onUploadProgress: (progressEvent) => {
+                    const uploadProgress = progressEvent.total ? Math.round((progressEvent.loaded / progressEvent.total) * 100) : 0;
+                    setProgress(uploadProgress);
+                },
+            });
+
+            setStatus("success");
+            setProgress(100);
+        } catch {
+            setStatus("error");
+            setProgress(0);
+        }
+    };
+
+    return {
+        status,
+        progress,
+        uploadFile,
+    };
+};
+
+export default useFileUpload;
