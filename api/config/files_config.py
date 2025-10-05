0b5dfde8a5add520c063ce253d1cda7cd860278b
TeKayy <th3tk.idk@gmail.com>
2025-10-05 15:23:54 +0200

    Removed no longer used paths from the files config.

--- api/config/files_config.py
+++ api/config/files_config.py
@@ -3,9 +3,6 @@ from pathlib import Path
 
 @dataclass(frozen=True)
 class FilesConfig:
-    network_config_intnets: Path = Path("data/network_configuration/intnets.local.json")
-    network_config_state_save: Path = Path("data/network_configuration/current_state.local.json")
-    network_config_snapshots: Path = Path("data/network_configuration/snapshots.local.json")
     network_config_presets: Path = Path("data/network_configuration/presets.json")
 
 FILES_CONFIG = FilesConfig()
