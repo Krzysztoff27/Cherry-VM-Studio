95e49784e0b3c81f9a7d1aa1bdd226b3fa1899fa
Krzysztoff27 <krzysztof.kolasinski27@outlook.com>
2025-10-14 00:52:45 +0200

    Updated initdb script with new table for iso files data storage.

--- backend/installer-files/docker/initdb/02-initdb.sql
+++ backend/installer-files/docker/initdb/02-initdb.sql
@@ -113,87 +113,6 @@ CREATE TABLE machine_snapshots_shares (
     FOREIGN KEY(recipient_uuid) REFERENCES administrators(uuid) ON DELETE CASCADE
 );
 
--- Virtual Machine object representation as tables of PGSQL composite types
-
--- Metadata
-CREATE TYPE machine_metadata AS (
-    tag VARCHAR(255) NOT NULL,
-    value VARCHAR(255) NOT NULL
-);
-
-CREATE TYPE group_metadata AS (
-    tag VARCHAR(255) NOT NULL,
-    value VARCHAR(255) NOT NULL
-);
-
-CREATE TYPE group_member_id_metadata AS (
-    tag VARCHAR(255) NOT NULL,
-    value VARCHAR(255) NOT NULL 
-);
-
--- Wrapper functions for setting tags of restricted "tag" value
-CREATE FUNCTION group_metadata(value VARCHAR)
-RETURNS group_metadata LANGUAGE sql IMMUTABLE AS $$
-    SELECT ROW('group', value)::group_metadata;
-$$;
-
-CREATE FUNCTION group_member_id_metadata(value VARCHAR)
-RETURNS group_member_id_metadata LANGUAGE sql IMMUTABLE AS $$
-    SELECT ROW('groupMemberId', value)::group_member_id_metadata;
-$$;
-
-CREATE FUNCTION additional_metadata(tag VARCHAR, value VARCHAR)
-RETURNS additional_metadata LANGUAGE sql IMMUTABLE AS $$
-    SELECT ROW(tag, value)::machine_metadata;
-$$;
-
--- Machine disks
-CREATE TYPE disk_type AS ENUM ("raw", "qcow2", "qed", "qcow", "luks", "vdi", "vmdk", "vpc", "vhdx");
-
-CREATE TYPE machine_disk AS (
-    name VARCHAR(255) NOT NULL,
-    filepath VARCHAR(255) NOT NULL,
-    size INT NOT NULL,
-    type disk_type
-);
-
--- Machine Network Interfaces
-CREATE TYPE machine_network_interfaces AS (
-    name VARCHAR(255) NOT NULL
-);
-
--- Machine Parameters
-CREATE TABLE machine_templates (
-    uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-
-    -- required metadata fields
-    group_metadata group_metadata NOT NULL,
-    group_member_id_metadata group_member_id_metadata, -- this one can be NULL as it is most commonly set during machine runtime
-
-    -- optional list of arbitrary metadata
-    additional_metadata machine_metadata[],
-
-    ram INT NOT NULL CHECK (ram > 0),
-    vcpu INT NOT NULL CHECK (vcpu > 0),
-    os_type VARCHAR(255),
-
-    disks machine_disk[] NOT NULL,
-
-    username VARCHAR(255),
-    password VARCHAR(255),
-
-    network_interfaces network_interface[]
-
-    -- constraints to enforce tag correctness
-    CONSTRAINT group_metadata_tag_check
-        CHECK ((group_metadata).tag = 'group')
-
-    CONSTRAINT group_member_id_tag_check
-        CHECK ((group_member_id_metadata).tag = 'groupMemberId')
-);
-
 CREATE INDEX administrators_idx ON administrators (uuid, username, email);
 CREATE INDEX clients_idx ON clients (uuid, username, email);
 CREATE INDEX roles_idx ON roles (uuid);
@@ -206,11 +125,6 @@ CREATE INDEX network_panel_states_idx ON network_panel_states(owner_uuid);
 CREATE INDEX network_snapshots_idx ON network_snapshots(uuid, owner_uuid);
 CREATE INDEX machine_snapshots_idx ON machine_snapshots(uuid, owner_uuid);
 CREATE INDEX machine_snapshots_shares_idx ON machine_snapshots_shares(snapshot_uuid, recipient_uuid);
-CREATE INDEX machine_templates_uuid_idx ON machine_templates (uuid);
-CREATE INDEX machine_templates_name_idx ON machine_templates (name);
-CREATE INDEX machine_templates_group_value_idx ON machine_templates ((group_metadata).value);
-CREATE INDEX machine_templates_group_member_id_idx ON machine_templates ((group_member_id_metadata).value);
-CREATE INDEX machine_templates_additional_metadata_idx ON machine_templates USING GIN (additional_metadata);
 CREATE INDEX iso_files_idx ON iso_files (uuid, name);
 
 
