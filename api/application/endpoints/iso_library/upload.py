3d97bf629fd8b34fdabc8445f788b9fd88c5bc76
TeKayy <th3tk.idk@gmail.com>
2025-10-14 01:16:30 +0200

    Debugging.

--- api/application/endpoints/iso_library/upload.py
+++ api/application/endpoints/iso_library/upload.py
@@ -32,7 +32,6 @@ async def __upload_iso_file__(current_user: DependsOnAdministrativeAuthenticatio
     try:
         try:
             uploaded_file = await upload_handler.handle(request)
-            logging.debug("ISO upload form: ", uploaded_file.form_data)
             form_data = CreateIsoRecordForm.model_validate_json(uploaded_file.form_data)
             
             creation_args = CreateIsoRecordArgs(
@@ -64,7 +63,8 @@ async def __upload_iso_file__(current_user: DependsOnAdministrativeAuthenticatio
         pass
     except HTTPException as e:
         raise e
-    except PydanticValidationError:
+    except PydanticValidationError as e:
+        logging.exception("Validation error during the ISO upload.")
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, 
             detail='Invalid JSON form structure'
